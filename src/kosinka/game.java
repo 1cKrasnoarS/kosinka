aace68f82d49148f8462d293f7e30b366e6964e7
1cKrasnoarS <1cKrasnoarS@mail.ru>
2016-08-26 17:24:42 +0300

    в

--- src/kosinka/game.java
+++ src/kosinka/game.java
@@ -52,20 +52,95 @@ public class game {
 	{}
 	//при отпускании левой кнопки мыши
 	public void mouseReleased(int mX,int mY)
-	{}
+	{
+		int nom=getNomKolodaPress(mX,mY);
+	if(nom==0)
+	{
+	vidacha();	
+
+
+
+
+	}
+
+	}
 	//определении стопки на какую нажали мышью
 	public int getNomKolodaPress(int mX,int mY)
 	{
-		return -1;		
+		int nom=1;
+	if((mX>=30)&&(mX<=(30+72))) nom=0;
+	if((mX>=140)&&(mX<=(140+72))) nom=1;
+	if((mX>=360)&&(mX<=(360+72))) nom=2;
+	if((mX>=470)&&(mX<=(470+72))) nom=3;
+	if((mX>=580)&&(mX<=(580+72))) nom=4;
+	if((mX>=690)&&(mX<=(690+72))) nom=5;
+
+	else if((mX>=130)&&(mX<=(700+72)))
+	{
+		
+	if((mX>=30)&&(mX<=110*7))
+	{
+		if(((mX-30)%110)<=72)
+		{
+			nom=(mX-30)/110;
+			nom+=6;
+			
+		}
+
+
+	}
+
+
+
+	}
+	return nom;
 	}
 	
 	//выдача карт из верхней левой стопки
 	private void vidacha()
-	{}
+	{
+		
+		if(stopki[0].size()>0)
+		{
+		int nom;	
+		if(pervVidacha==true)
+		{
+		nom=(int)(Math.random()*stopki[0].size());	
+
+		}
+
+		else
+		{
+			
+		nom=stopki[0].size()-1;
+
+
+
+		}
+
+		karta getKarta=stopki[0].get(nom);
+		getKarta=stopki[0].get(nom);
+
+		}
+
+		}
 	
 	//старт игры новая игра
 	public void start()
-	{}
+	{
+		for (int i=0;i<13;i++)	
+		{
+			
+		stopki[i].clear();
+
+		}
+
+		load();
+
+		endGame=false;
+		pervVidacha=true;
+
+		}
 	
 	//загрузка изображений из колоды
 	private void load()	
@@ -79,7 +154,34 @@ public class game {
 
 	//метод отрисовки всех стопок карт
 	public void drawKoloda(Graphics gr)
-	{}
+	{
+
+		if(stopki[0].size()>0)
+		{
+			
+			stopki[0].get(stopki[0].size()-1).draw(gr);
+			
+		}
+	if(stopki[1].size()>1)
+	{
+		stopki[1].get(stopki[1].size()-2).draw(gr);
+		stopki[1].get(stopki[1].size()-1).draw(gr);
+		
+		
+		
+		
+	}
+		
+	else if(stopki[1].size()==1)
+	{
+		stopki[1].get(stopki[1].size()-1).draw(gr);	
+
+
+
+	}
+
+
+
 	
 	
 	
